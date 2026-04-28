15176d7603ba98e4725a6d072ecad7bf13370d34
ilya-shsherbenko <i_shherbenko@mail.ru>
2026-04-28 22:53:48 +0300

    feat: add Firebase-backed admin panel and dynamic catalog

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAp7-Y95d8FWF-h8T88dFoHgRbNXA1ZaQc",
+  authDomain: "flowerroradmin.firebaseapp.com",
+  projectId: "flowerroradmin",
+  storageBucket: "flowerroradmin.firebasestorage.app",
+  messagingSenderId: "292864754324",
+  appId: "1:292864754324:web:d256567a4a4f6d64b2aa81",
+  measurementId: "G-VYDERSGG0S"
+};
